eecb5cdfcc30404e0138aae9ccc187b79998956a
danial sadri <danielsadri01@gmail.com>
2023-08-13 17:55:20 +0330

    add most_popular_posts def

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 from blog.models import Post, Comment
 
 register = template.Library()
@@ -17,6 +18,11 @@ def total_comments():
 @register.simple_tag()
 def last_post_date():
     return Post.published.last().publish
+
+
+@register.simple_tag()
+def most_popular_posts(count=5):
+    return Post.published.annotate(comments_count=Count('comments')).order_by('-comments_count')[:count]
 
 
 @register.inclusion_tag('partials/latest_posts.html')
